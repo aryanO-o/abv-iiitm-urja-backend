0bf2b5e9bd5d4a15d3cb175e540c2bac9842a380
Aryan <aryandadhich.ad@gmail.com>
2022-07-17 20:50:03 +0530

    changed index.js ka port variable

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(express.json());
 // using cors to communicate between different sites initially all sites can use data so just cors()
 app.use(cors());
 // making a variable to store the port and since we will be hosting it so using process.env.PORT and to work locally adding 8000 port.
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 // initialization of / route
 app.get('/', (req, res) =>{
